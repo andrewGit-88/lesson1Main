4349071d0150ae2de9adac26f6ce8998e9bfee1f
andrewm <andrewmarty88@yahoo.ca>
2023-02-04 15:32:58 -0500

    this is a test on a different branch member

--- Java/lesson1Main/src/main/java/module9/CheckCommitAndPush.java
+++ Java/lesson1Main/src/main/java/module9/CheckCommitAndPush.java
@@ -3,5 +3,6 @@ package module9;
 public class CheckCommitAndPush {
     public static void main(String[] args) {
         System.out.println("sad");
+        System.out.println("master switch");
     }
 }
