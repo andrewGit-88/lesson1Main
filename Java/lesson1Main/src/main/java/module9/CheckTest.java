ca196130046cb0b637fcf70812164b66542866bf
andrewm <andrewmarty88@yahoo.ca>
2023-02-04 17:23:47 -0500

    test - create repository

--- Java/lesson1Main/src/main/java/module9/CheckTest.java
+++ Java/lesson1Main/src/main/java/module9/CheckTest.java
@@ -4,6 +4,7 @@ public class CheckTest {
     public static void main(String[] args) {
         System.out.println("check test");
         System.out.println("also commit additional test");
+        System.out.println("will add additional student test AM");
     }
 
 }
