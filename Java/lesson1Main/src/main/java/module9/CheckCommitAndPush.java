c3cf29af75ddd753587f8094c007417ef757091e
andrewm <andrewmarty88@yahoo.ca>
2023-02-04 15:10:32 -0500

    this is a test

--- Java/lesson1Main/src/main/java/module9/CheckCommitAndPush.java
+++ Java/lesson1Main/src/main/java/module9/CheckCommitAndPush.java
@@ -3,5 +3,9 @@ package module9;
 public class CheckCommitAndPush {
     public static void main(String[] args) {
         System.out.println("sad");
+        System.out.println("this is now Andy's Branch");
+
+
+
     }
 }
