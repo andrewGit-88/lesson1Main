e47750db80f03ed2c4931db9c57f785564d13cb7
andrewm <andrewmarty88@yahoo.ca>
2023-02-04 17:15:01 -0500

    test - commit - secondary test

--- Java/lesson1Main/src/main/java/module9/CheckTest.java
+++ Java/lesson1Main/src/main/java/module9/CheckTest.java
@@ -3,5 +3,7 @@ package module9;
 public class CheckTest {
     public static void main(String[] args) {
         System.out.println("check test");
+        System.out.println("also commit additional test");
     }
+
 }
