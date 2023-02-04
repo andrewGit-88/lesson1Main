f224c03c44733e363e9fc6ae640e955a53d7b77e
andrewm <andrewmarty88@yahoo.ca>
2023-02-04 15:22:34 -0500

    this is a test on a different branch member

--- Java/lesson1Main/src/main/java/module9/CheckCommitAndPush.java
+++ Java/lesson1Main/src/main/java/module9/CheckCommitAndPush.java
@@ -4,8 +4,6 @@ public class CheckCommitAndPush {
     public static void main(String[] args) {
         System.out.println("sad");
         System.out.println("this is now Andy's Branch");
-        System.out.println("we check it out");
-        System.out.println("fddsafasdf");
 
 
 
