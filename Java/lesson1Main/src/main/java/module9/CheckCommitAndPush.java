24c1cb135865d90318dcdd103106f6e986515a7b
andrewm <andrewmarty88@yahoo.ca>
2023-02-04 15:51:46 -0500

    test newAndy

--- Java/lesson1Main/src/main/java/module9/CheckCommitAndPush.java
+++ Java/lesson1Main/src/main/java/module9/CheckCommitAndPush.java
@@ -3,7 +3,14 @@ package module9;
 public class CheckCommitAndPush {
     public static void main(String[] args) {
         System.out.println("sad");
-        System.out.println("master switch, additional test");
+        System.out.println("this is now Andy's Branch");
+        System.out.println("this should be merged");
+        System.out.println("part of it");
+
+
+
+
+
 
     }
 }
